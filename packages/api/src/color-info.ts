719cf6a49fed3cf5969225a867cfdb78f64b0c79
Vladyslav Zhukovskyi <vzhukovs@redhat.com>
2026-03-16 15:29:07 +0200

    refactor: align HC theme identifiers with VS Code convention
    Signed-off-by: Vladyslav Zhukovskyi <vzhukovs@redhat.com>

--- packages/api/src/color-info.ts
+++ packages/api/src/color-info.ts
@@ -19,8 +19,8 @@
 export interface ColorDefinition {
   dark: string;
   light: string;
-  darkHc?: string;
-  lightHc?: string;
+  hcDark?: string;
+  hcLight?: string;
 }
 
 export interface ColorInfo {
